ca4e027efcc6dade94791026c28e2698543a9bf5
backhousse <backhouselee2001@gmail.com>
2025-08-14 09:50:24 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -194,7 +194,11 @@ IMPORT_INPUT.addEventListener('change', (e)=>{
 });
 
 // PWA
-if('serviceWorker' in navigator){ window.addEventListener('load', ()=> navigator.serviceWorker.register('./sw.js')); }
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () =>
+    navigator.serviceWorker.register('./sw.js', { updateViaCache: 'none' })
+  );
+}
 
 // Buttons
 TODAY_BTN.addEventListener('click', ()=>{
